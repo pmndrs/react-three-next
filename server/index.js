c2b397d77293c994de0d54ef891bfb1c28eb5296
onirenaud <renaud.rohlinger@gmail.com>
2020-10-26 22:59:23 +0900

    fix dev

--- server/index.js
+++ server/index.js
@@ -1,25 +1,25 @@
-import * as express from "express";
-import next from "next";
+import express from 'express'
+import next from 'next'
 
-const { PORT, NODE_ENV } = process.env;
-const port = PORT ? parseInt(PORT, 10) : 3000;
-const dev = NODE_ENV !== "production";
-const app = next({ dev });
-const handle = app.getRequestHandler();
+const { PORT, NODE_ENV } = process.env
+const port = PORT ? parseInt(PORT, 10) : 3000
+const dev = NODE_ENV !== 'production'
+const app = next({ dev })
+const handle = app.getRequestHandler()
 
 app.prepare().then(() => {
-  const server = express();
+  const server = express()
 
-  server.get("*", (req, res) => {
-    return handle(req, res);
-  });
+  server.get('*', (req, res) => {
+    return handle(req, res)
+  })
 
   server.listen(port, (err) => {
     if (err) {
-      throw err;
+      throw err
     }
 
     // eslint:disable-next-line:no-console
-    console.log(`> Ready on http://localhost:${port}`);
-  });
-});
+    console.log(`> Ready on http://localhost:${port}`)
+  })
+})
