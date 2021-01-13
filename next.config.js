e8e9f58fcfac52265451093ffea3f9cd722ea6f0
onirenaud <renaud.rohlinger@gmail.com>
2021-01-14 01:40:23 +0900

    set back to v4 for security and temporary removed custom server

--- next.config.js
+++ next.config.js
@@ -3,23 +3,35 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 })
 
+// custom transpile module
+const debug = true
+let start = null
+let i = 0
+
 const match = (path) => {
   if (!path.includes('three/examples/jsm')) return false
-  console.info(`transpiled: ${path}`)
+  if (debug) {
+    // should be around 3/4 seconds the first time and then 200ms after using Webpack 5 built-in loader cache
+    let end = start ? new Date() - start : 0
+    console.log(
+      `\x1b[37m`,
+      `🚄 ${end}ms - The transpilation ${path} in process`
+    )
+    if (i === 1) {
+      start = new Date()
+    }
+    i++
+  }
   return true
 }
 
 const withOffline = require('next-offline')
 const withTM = require('next-transpile-modules')(
-  ['three', '@react-three/drei', '@react-three/postprocessing'],
-  { debug: false, unstable_webpack5: true, match } // symlink-caused loops which cause memory to get bloated exponentially.
+  ['three', '@react-three/drei'], // '@react-three/postprocessing'
+  { debug: debug, unstable_webpack5: false, match } // symlink-caused loops which cause memory to get bloated exponentially.
 )
-console.log(withTM)
+
 const nextConfig = {
-  i18n: {
-    locales: ['en-US'],
-    defaultLocale: 'en-US',
-  },
   webpack(config) {
     config.module.rules.push(
       { test: /react-spring/, sideEffects: true }, // prevent vercel to crash when deploy
@@ -31,6 +43,14 @@ const nextConfig = {
     )
     return config
   },
+}
+
+// manage i18n
+if (process.env.EXPORT !== 'true') {
+  nextConfig.i18n = {
+    locales: ['en-US'],
+    defaultLocale: 'en-US',
+  }
 }
 
 module.exports = plugins(
