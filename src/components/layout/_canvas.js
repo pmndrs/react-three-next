306d4eac933f78b26ba5d55729a7d99ae94e8d75
onirenaud <renaud.rohlinger@gmail.com>
2021-02-07 21:23:46 +0900

    precompile scene to improve startup and update dependencies

--- src/components/layout/_canvas.js
+++ src/components/layout/_canvas.js
@@ -1,7 +1,7 @@
 import { Canvas } from 'react-three-fiber'
 import { Perf } from 'r3f-perf'
 import useStore from '@/helpers/store'
-import { OrbitControls } from '@react-three/drei'
+import { OrbitControls, Preload } from '@react-three/drei'
 import { a, useSpring } from '@react-spring/three'
 import { EffectComposer, Vignette } from '@react-three/postprocessing'
 // enable shader editor
@@ -25,6 +25,7 @@ const LCanvas = ({ children }) => {
         useStore.setState({ events })
       }}
     >
+      <Preload all />
       <Bg />
       <Perf openByDefault trackGPU={true} position={'bottom-right'} />
       <OrbitControls />
