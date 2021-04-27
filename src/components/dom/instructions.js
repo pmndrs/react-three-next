26cf4351f264e698f65007b1936f2a35bcca1920
onirenaud <renaud.rohlinger@gmail.com>
2021-04-27 18:14:18 +0900

    added instructions and fix tailwind

--- src/components/dom/instructions.js
+++ src/components/dom/instructions.js
@@ -1,13 +1,13 @@
 export default function Instructions() {
   return (
-    <h2
+    <div
+      className='absolute top-8 left-1/2 text-gray-50 px-4 py-2 rounded-xl max-w-lg shadow-xl transform -translate-x-1/2'
       style={{ backgroundColor: 'rgb(27, 30, 40)' }}
-      className='fixed z-10 top-8 left-1/2 text-gray-50 px-4 py-2 transform -translate-x-1/2 rounded-xl max-w-lg shadow-xl'
     >
       <p className='mb-8'>
-        This is a minimal example of Nextjs + Threejs. A11y is included to
-        provide a minimum of accessibility. If you click on the cube you will
-        navigate to a dummy /box page.
+        This is a minimal starter for Nextjs + Threejs. A11y is included to
+        provide a basic layer of accessibility. If you click on the cube it will
+        navigate to a dummy '/box' page.
       </p>
       <pre>
         Step 1 - <span style={{ color: 'rgb(84, 90, 114)' }}>update:</span>
@@ -35,6 +35,6 @@ export default function Instructions() {
           @components/dom/instructions.jsx
         </bold>
       </pre>
-    </h2>
+    </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,4 @@
 module.exports = {
-  // crash with next see https://github.com/tailwindlabs/tailwindcss/issues/4081
   // mode: 'jit',
   purge: ['./src/pages/**/*.js', './src/components/**/*.js'], // remove unused styles in production
   darkMode: 'media', // or 'media' or 'class'
