923a210d550cf6b87c620e8d5c1ea5253ede45af
onirenaud <renaud.rohlinger@gmail.com>
2021-05-30 21:35:12 +0900

    fix tailwind and added jit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 module.exports = {
-  // mode: 'jit',
-  purge: ['./src/pages/**/*.jsx', './src/components/**/*.jsx'], // remove unused styles in production
+  mode: 'jit',
+  purge: [
+    './src/pages/**/*.{js,ts,jsx,tsx}',
+    './src/components/**/*.{js,ts,jsx,tsx}',
+  ], // remove unused styles in production
   darkMode: 'media', // or 'media' or 'class'
   theme: {
     extend: {},
