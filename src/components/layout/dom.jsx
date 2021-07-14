529692ea013e46f2633940ade35f9d04a6d3c59f
njm222 <nathan.john.menezes@gmail.com>
2021-07-14 13:12:16 +0530

    useLayoutEffect for dom setState

--- src/components/layout/dom.jsx
+++ src/components/layout/dom.jsx
@@ -1,9 +1,11 @@
 import useStore from '@/helpers/store'
-import { useRef } from 'react'
+import { useLayoutEffect, useRef } from 'react'
 
 const Dom = ({ children }) => {
   const ref = useRef(null)
-  useStore.setState({ dom: ref })
+  useLayoutEffect(() => {
+    useStore.setState({ dom: ref })
+  }, [])
   return (
     <div
       className='absolute top-0 left-0 z-10 w-screen h-screen overflow-hidden dom'
