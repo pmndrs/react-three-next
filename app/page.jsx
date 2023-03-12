48f34688bb70f58fc4a2ce2b2f58b76d3f2b4192
Renaud Rohlinger <renaud.rohlinger@gmail.com>
2023-03-12 20:31:19 +0900

    improve access

--- app/page.jsx
+++ app/page.jsx
@@ -26,9 +26,9 @@ const Common = dynamic(() => import('@/components/canvas/View').then((mod) => mo
 export default function Page() {
   return (
     <>
-      <div className='mx-auto flex w-full flex-col flex-wrap items-center p-12 md:flex-row  lg:w-4/5'>
+      <div className='mx-auto flex w-full flex-col flex-wrap items-center md:flex-row  lg:w-4/5'>
         {/* jumbo */}
-        <div className='flex w-full flex-col items-start justify-center text-center md:w-2/5 md:text-left'>
+        <div className='flex w-full flex-col items-start justify-center p-12 text-center md:w-2/5 md:text-left'>
           <p className='w-full uppercase'>Next + React Three Fiber</p>
           <h1 className='my-4 text-5xl font-bold leading-tight'>Next 3D Starter</h1>
           <p className='mb-8 text-2xl leading-normal'>A minimalist starter for React, React-three-fiber and Threejs.</p>
@@ -47,7 +47,7 @@ export default function Page() {
       <div className='mx-auto flex w-full flex-col flex-wrap items-center p-12 md:flex-row  lg:w-4/5'>
         {/* first row */}
         <div className='relative h-48 w-full p-6 sm:w-1/2 md:my-12 md:mb-40'>
-          <h3 className='mb-3 text-3xl font-bold leading-none text-gray-800'>Events are propagated</h3>
+          <h2 className='mb-3 text-3xl font-bold leading-none text-gray-800'>Events are propagated</h2>
           <p className='mb-8 text-gray-600'>Drag, scroll, pinch, and rotate the canvas to explore the 3D scene.</p>
         </div>
         <div className='relative my-12 h-48 w-full p-6 sm:w-1/2 md:mb-40'>
@@ -68,7 +68,7 @@ export default function Page() {
           </View>
         </div>
         <div className='w-full p-6 sm:w-1/2'>
-          <h3 className='mb-3 text-3xl font-bold leading-none text-gray-800'>Dom and 3D are synchronized</h3>
+          <h2 className='mb-3 text-3xl font-bold leading-none text-gray-800'>Dom and 3D are synchronized</h2>
           <p className='mb-8 text-gray-600'>
             3D Divs are renderer through the View component. It uses gl.scissor to cut the viewport into segments. You
             tie a view to a tracking div which then controls the position and bounds of the viewport. This allows you to
