6adc6461771760df348ad8288dc4d21c89364b7d
Milosz Szekiel <12242002+mszekiel@users.noreply.github.com>
2023-04-10 17:19:36 +0200

    refactor: remove redundant key creation

--- src/templates/Shader/Shader.jsx
+++ src/templates/Shader/Shader.jsx
@@ -15,11 +15,6 @@ const ShaderImpl = shaderMaterial(
   fragment,
 )
 
-// This is the 🔑 that HMR will renew if this file is edited
-// It works for THREE.ShaderMaterial as well as for drei/shaderMaterial
-// @ts-ignore
-ShaderImpl.key = THREE.MathUtils.generateUUID()
-
 extend({ ShaderImpl })
 
 // eslint-disable-next-line react/display-name
