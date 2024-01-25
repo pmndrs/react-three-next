426094006c81334adb0cff716287834e87fc3ce4
— <drcmda@gmail.com>
2024-01-25 13:06:37 +0100

    fix: zoom

--- src/components/canvas/Common.jsx
+++ src/components/canvas/Common.jsx
@@ -10,7 +10,7 @@ export default function Common({ color, controls }) {
       <directionalLight decay={0} position={[10, 10, 5]} intensity={5} castShadow />
       <pointLight decay={0} position={[-10, -0, -10]} color='blue' intensity={3} />
       <PerspectiveCamera makeDefault fov={40} position={[0, 0, 6]} />
-      {controls && <OrbitControls />}
+      {controls && <OrbitControls enableZoom={false} />}
     </>
   )
 }
